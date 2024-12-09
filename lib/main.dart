9d659e47a239e7e2b398032cebc88cf02696d3d9
Muhammad Ali Nizami <kustalics@gmail.com>
2024-12-09 12:45:44 +0500

    Nested Row/Column Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.green,
         appBar: AppBar(
           backgroundColor: Colors.blue,
-          title: Text(
+          title: const Text(
             "First Class",
             style: TextStyle(
                 fontSize: 40.5,
@@ -22,25 +22,46 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.bold),
           ),
           centerTitle: true,
-          leading: Icon(
+          leading: const Icon(
             Icons.arrow_back,
             color: Colors.white,
           ),
           actions: [
-            Icon(Icons.notifications),
-            Icon(Icons.notifications),
-            Icon(Icons.notifications),
+            const Icon(Icons.notifications),
+            const Icon(Icons.notifications),
+            const Icon(Icons.notifications),
           ],
         ),
-        body: Center(
-          child: Text(
-            "Ali",
-            style: TextStyle(fontSize: 40),
-          ),
+        body: const Column(
+          children: [
+            Text(
+              "Lorem Ipsum has been the industry's standard ",
+              style: TextStyle(fontSize: 20, color: Colors.black),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type ",
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.add),
+                Icon(Icons.add),
+                Icon(Icons.add),
+                Icon(Icons.add),
+              ],
+            )
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
-          child: Icon(
+          child: const Icon(
             Icons.add,
             color: Colors.white,
           ),
