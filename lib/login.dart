bc466ebc3f0dc44ad65f407baa07681b43a6f9b0
Muhammad Ali Nizami <kustalics@gmail.com>
2024-12-17 12:44:09 +0500

    Validation Done

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:string_validator/string_validator.dart';
 class LoginView extends StatelessWidget {
-  const LoginView({super.key});
+  LoginView({super.key});
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController pwdController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,46 +24,58 @@ class LoginView extends StatelessWidget {
       body: Column(
         children: [
           SizedBox(
-            height: 30,
+            height: 10,
           ),
-          TextField(
-            decoration: InputDecoration(
-                label: Text("Email"),
-                hintText: 'test@gmail.com',
-                prefixIcon: Icon(Icons.email),
-                suffixIcon: Icon(Icons.email),
-                filled: true,
-                fillColor: Colors.blue.withOpacity(0.5),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.green, width: 5)),
-                disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.green, width: 5)),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.green, width: 5))),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              controller: emailController,
+              maxLines: 4,
+              decoration: InputDecoration(
+                  label: Text("Email"),
+                  hintText: 'test@gmail.com',
+                  prefixIcon: Icon(Icons.email),
+                  suffixIcon: Icon(Icons.email),
+                  filled: true,
+                  fillColor: Colors.blue.withOpacity(0.5),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.green, width: 5)),
+                  disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.green, width: 5)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.green, width: 5))),
+            ),
           ),
           SizedBox(
-            height: 30,
+            height: 10,
           ),
-          TextField(
-            decoration: InputDecoration(
-                label: Text("Passwrod"),
-                hintText: 'Password must be 6 or more characters.',
-                prefixIcon: Icon(Icons.lock),
-                suffixIcon: Icon(Icons.lock),
-                filled: true,
-                fillColor: Colors.blue.withOpacity(0.5),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.green, width: 5)),
-                disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.green, width: 5)),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.green, width: 5))),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              controller: pwdController,
+              keyboardType: TextInputType.emailAddress,
+
+              obscureText: true,
+              decoration: InputDecoration(
+                  label: Text("Passwrod"),
+                  hintText: 'Password must be 6 or more characters.',
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.lock),
+                  filled: true,
+                  fillColor: Colors.blue.withOpacity(0.5),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.green, width: 5)),
+                  disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.green, width: 5)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.green, width: 5))),
+            ),
           ),
           SizedBox(
             height: 20,
@@ -75,7 +90,30 @@ class LoginView extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          ElevatedButton(onPressed: () {}, child: Text("Login"))
+          ElevatedButton(
+              onPressed: () {
+                if (emailController.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Email cannot be empty.")));
+                  return;
+                }
+                if (!emailController.text.isEmail) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Kindly enter valid email.")));
+                  return;
+                }
+                if (pwdController.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Password cannot be empty.")));
+                  return;
+                }
+                if (pwdController.text.length < 6) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Password must be 6 or more characters.")));
+                  return;
+                }
+              },
+              child: Text("Login"))
         ],
       ),
     );
